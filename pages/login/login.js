e2953bc7257f718ed90c6b3dae9bcfd52035b7d1
simonj1704 <simo915m@stud.kea.dk>
2023-10-04 11:14:16 +0200

    Fixed signup and added settings.js

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,49 +1,57 @@
-export function initLogin(){
-    document.getElementById("login-form").style.display="block"
-    document.getElementById("close").addEventListener("click",()=>{
-        document.getElementById("login-form").style.display="none"
-    })
+import { API_URL} from "../../settings.js";
+import { handleHttpErrors, makeOptions } from "../../utils.js";
 
-    const loginForm = document.getElementById('loginForm');
-    const loggedInMessage = document.getElementById('loggedInMessage');
-    
+export function initLogin() {
+  document.getElementById("login-form").style.display = "block";
+  document.getElementById("close").addEventListener("click", () => {
+    document.getElementById("login-form").style.display = "none";
+  });
 
-    loginForm.addEventListener('submit', async (e) => {
-        e.preventDefault();
-        const formData = new FormData(loginForm);
-        const username = formData.get('username');
-        const password = formData.get('password');
+  const loginForm = document.getElementById("loginForm");
 
-        try {
-            const response = await fetch('http://localhost:8080/api/auth/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ username, password }),
-            });
+  loginForm.addEventListener("submit", async (e)=> {
+    e.preventDefault();
+    login()
+});
+}
 
-            if (response.ok) {
-                const data = await response.json();
-                const token = data.token;
-                const loggedInUser = data.username;
-                console.log(data.token);
-                const roles = data.roles;
+async function login() {
+  const formData = new FormData(loginForm);
+  const username = formData.get("username");
+  const password = formData.get("password");
+  const loggedInMessage = document.getElementById("loggedInMessage");
 
-                // Display the logged-in message
-                loggedInMessage.textContent = `Logged in as ${loggedInUser}`;
-               // Store the token in localStorage
-                localStorage.setItem('token', token);
-                localStorage.setItem('username',loggedInUser)
-                localStorage.setItem('roles', roles);
-                document.getElementById('loggedInAs').innerHTML=`Logged in as ${localStorage.getItem('username')}<a href="signOut" data-navigo style="margin-left: 10px; margin-right: 10px; font-size: 12px;">Sign Out</a>`
 
-            } else {
-                loggedInMessage.textContent = 'Login failed. Please check your credentials.';
-            }
-        } catch (error) {
-            console.error('Error:', error);
-            loggedInMessage.textContent = 'An error occurred during login.';
-        }
-    });
+  try {
+    const response = await fetch(
+      API_URL + "/auth/login",
+      makeOptions("POST", { username, password }, false)
+    );
+
+    if (response.ok) {
+      const data = await response.json();
+      const token = data.token;
+      const loggedInUser = data.username;
+      console.log(data.token);
+      const roles = data.roles;
+
+      // Display the logged-in message
+      loggedInMessage.textContent = `Logged in as ${loggedInUser}`;
+      // Store the token in localStorage
+      localStorage.setItem("token", token);
+      localStorage.setItem("username", loggedInUser);
+      localStorage.setItem("roles", roles);
+      document.getElementById(
+        "loggedInAs"
+      ).innerHTML = `Logged in as ${localStorage.getItem(
+        "username"
+      )}<a href="signOut" data-navigo style="margin-left: 10px; margin-right: 10px; font-size: 12px;">Sign Out</a>`;
+    } else {
+      loggedInMessage.textContent =
+        "Login failed. Please check your credentials.";
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    loggedInMessage.textContent = "An error occurred during login.";
+  }
 }
