6efbe17cc4981a7a351bd54bd65def08dd76e31b
mikljohsn <114925102+mikljohsn@users.noreply.github.com>
2023-10-05 14:13:24 +0200

    it works

--- pages/addFilms/addFilms.js
+++ pages/addFilms/addFilms.js
@@ -1,4 +1,4 @@
-import { sanitizeStringWithTableRows, handleHttpErrors } from "../../utils.js"
+import { sanitizeStringWithTableRows, handleHttpErrors, makeOptions} from "../../utils.js"
 
 const url = "http://localhost:8080/api/films"
 
@@ -6,6 +6,7 @@ const omdbUrl= "http://www.omdbapi.com/?apikey=6dfe795d&plot=full&i="
 
 export function initAddFilm(){
     document.querySelector("#inspect-btn").addEventListener("click", async () => inspectFilm())
+    document.querySelector("#add-film-btn").addEventListener("click", async () => addFilm())
 }
 
 async function inspectFilm(){
@@ -29,4 +30,28 @@ async function inspectFilm(){
 
     
     
+}
+
+async function addFilm(){
+    const filmId = document.querySelector("#film-input").value
+    const token = localStorage.getItem('token');
+    try{
+
+        if (!token) {
+            console.error('Token not found. Please log in.');
+            return;
+        }
+
+        const response = await fetch(url + "/" + filmId, makeOptions("POST", null, true))
+        
+        if(response.ok){
+            alert("GODT")
+        } else {
+            alert("LORT")
+        }
+        
+        
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
 }
